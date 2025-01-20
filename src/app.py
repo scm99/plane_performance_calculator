7adff2c90b111e3a0475cbd86e6a819f16aed985
Sara Marques <scmarques99@gmail.com>
2025-01-20 19:06:21 +0100

    feat: launches the app

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from view import UiForm
+import sys
+
+# Generate the app
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QMainWindow()
+
+# Setup Window
+ui = UiForm()
+ui.setupUi(window)
+window.show()
+
+# Styling the app
+#app.setStyleSheet(Path('style.qss').read_text())
+
+# Start the event loop.
+app.exec()
